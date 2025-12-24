7d07d9bd1fe3a61b84f098fd861eb50804ecf5ce
Owen Seropian <owenseropian@Owens-MacBook-Pro-2.local>
2025-12-24 01:25:10 -0600

    added shaders & VBO,VBA,EBO and added dodecahedron with vertices

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,21 @@ const char* WINDOW_NAME = "OPENGL WINDOW";
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
-void framebuffer_size_callback(GLFWwindow* window, int width = WINDOW_WIDTH, int height = WINDOW_HEIGHT)
+const char *vertexShaderSource = "#version 410 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 }
@@ -58,18 +72,110 @@ void close(GLFWwindow* &window)
     glfwTerminate();
 }
 
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
     GLFWwindow* window;
-
     if (!init_window(window)) {
         close(window);
         return -1;
     }
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //vertex shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    //fragment shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    //shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glUseProgram(shaderProgram);
+    //delete redundant shaders after linking
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader); 
+
+    //dodecahedron
+    float vertices[] = {
+    -0.5f, -0.5f, 0.5f,
+    0.5f, -0.5f, 0.5f,
+    -0.5f, 0.5f, 0.5f,
+    0.5f, 0.5f, 0.5f,
+    -0.5f, -0.5f, -0.5f,
+    0.5f, -0.5f, -0.5f,
+    -0.5f, 0.5f, -0.5f,
+    0.5f, 0.5f, -0.5f,
+    };
+    unsigned int indices[] = { 
+    0, 1, 2,
+    2, 1, 3,
+    5, 4, 7,
+    7, 4, 6,
+    4, 0, 6,
+    6, 0, 2,
+    1, 5, 3,
+    3, 5, 7,
+    2, 3, 6,
+    6, 3, 7,
+    4, 5, 0,
+    0, 5, 1
+    }; 
+    
+
+    //vertex buffer object
+    unsigned int VBO;
+    glGenBuffers(1, &VBO); 
+
+    //vertex array object
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO); 
+
+    //Element buffer object
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     while(!glfwWindowShouldClose(window))
     {
+        //input
+        processInput(window);
+
+        //rendering
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+        glUseProgram(shaderProgram);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+        //check and call events and swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();    
     }
@@ -77,3 +183,4 @@ int main()
     close(window);
     return 0;
 }
+
