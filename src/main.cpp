938cee57f6d68645e804666f5ec855051b5311c6
Owen Seropian <owenseropian@Owens-MacBook-Pro-2.local>
2025-12-27 16:17:15 -0600

    implementing starting angle for circles

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,33 @@ const int WINDOW_HEIGHT = 600;
 const char *vertexCodeString = "./shaders/shader.vert";
 const char *fragmentCodeString = "./shaders/shader.frag";
 
+float find_angle(fftw_complex arr){
+    return std::atan2(arr[1], arr[0]);
+}
+
+float test_func(float x){
+    return x + (float)std::pow(x, 2);
+}
+
+fftw_complex *fft_test(int N, int direction){
+    fftw_complex *in, *out;
+    fftw_plan p;
+
+    in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+    p = fftw_plan_dft_1d(N, in, out, direction, FFTW_ESTIMATE);
+
+    for (int x = 0; x < N; x++){
+        in[x][0] = test_func(float(x)/N);
+    }
+
+    fftw_execute(p);
+
+    fftw_destroy_plan(p);
+    fftw_free(in); 
+    return out;
+}
+
 class Circle
 {
 public:
@@ -55,6 +82,8 @@ public:
         windowWidth  = w;
         windowHeight = h;
 
+        output = fft_test(10, FFTW_FORWARD);
+
         myShader.init(vsFile, fsFile);
         setupCircleMesh();
         setupInstanceBuffer();
@@ -64,6 +93,9 @@ public:
     void setCircles(const std::vector<Circle>& c)
     {
         circles = c;
+        for (int i = 0; i < c.size(); i++){
+            printf("circle %i, pos: %f, %f, radius: %f\n", i, c[i].position[0], c[i].position[1], c[i].radius);
+        }
         updateInstanceBuffer();
     }
 
@@ -94,13 +126,20 @@ private:
     void setupCircleMesh()
     {
         std::vector<float> verts;
-        verts.reserve((SEGMENT_NUMBER + 1) * 3);
+        verts.reserve((SEGMENT_NUMBER + 2) * 3);
 
         float twoPi = 2.0f * static_cast<float>(M_PI);
 
+        verts.push_back(0.0f);
+        verts.push_back(0.0f);
+        verts.push_back(0.0f);
+
+        float starting_angle = find_angle(output[3]);
+        printf("vertex, %f, %f, starting angle %f\n", output[3][0], output[3][1], starting_angle);
+
         for (int i = 0; i <= SEGMENT_NUMBER; i++)
         {
-            float a = twoPi * i / SEGMENT_NUMBER;
+            float a = twoPi * i / SEGMENT_NUMBER + starting_angle;
             verts.push_back(std::cos(a));
             verts.push_back(std::sin(a));
             verts.push_back(0.0f);
@@ -112,6 +151,8 @@ private:
         glGenBuffers(1, &meshVBO);
 
         glBindVertexArray(VAO);
+
+        printf("vertz size %i\n", verts.size());
 
         glBindBuffer(GL_ARRAY_BUFFER, meshVBO);
         glBufferData(GL_ARRAY_BUFFER,
@@ -181,6 +222,7 @@ private:
     GLuint meshVBO = 0;
     GLuint instanceVBO = 0;
     Shader myShader;
+    fftw_complex *output;
 
     int vertexCount = 0;
     int windowWidth = 1;
@@ -248,166 +290,6 @@ void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-}
-
-float test_func(float x){
-    return x + (float)std::pow(x, 2);
-}
-
-fftw_complex *fft_test(int N){
-    fftw_complex *in, *out;
-    fftw_plan p;
-
-    in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-    p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-
-    for (int x = 0; x < N; x++){
-        in[x][0] = test_func(float(x/float(N)));
-    }
-
-    fftw_execute(p);
-
-    fftw_destroy_plan(p);
-    fftw_free(in); 
-    return out;
-}
-
-void storeCode()
-{
-    //idk what this code does but imma just store it
-
-    int num_frequencies = 10;
-
-    fftw_complex *test;
-    test = fft_test(num_frequencies);
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j<2; j++){
-            printf("%f ", test[j][i]);
-        }
-        printf("\n");
-    }
-
-    GLFWwindow* window;
-    if (!init_window(window)) {
-        close_window(window);
-        //return -1;
-    }
-
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    Shader myShader;
-    myShader.init(vertexCodeString, fragmentCodeString);
-
-    //dodecahedron
-    const float gr = (1 + std::sqrt(5)) / 2;
-    float vertices[(num_frequencies + 1) * 3];
-    unsigned int indices[num_frequencies * 2];
-    vertices[0] = 0;
-    vertices[1] = 0;
-    vertices[2] = 0;
-    for (int i = 0; i < 10; i++){
-        vertices[(i+1)*3] = test[i][0] + vertices[(i+1)*3 - 2];
-        vertices[(i+1)*3+1] = test[i][1] + vertices[(i+1)*3 - 1];
-        vertices[(i+1)*3+2] = 0;
-
-        indices[i*2] = i;
-        indices[i*2+1] = i+1;
-    }
-    
-
-    //vertex buffer object
-    unsigned int VBO;
-    glGenBuffers(1, &VBO); 
-
-    //vertex array object
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO); 
-
-    //Element buffer object
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    while(!glfwWindowShouldClose(window))
-    {
-        //input
-        processInput(window);
-
-        //rendering
-        //clear the colorbuffer
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-        //activate shader
-        myShader.use();
-
-        glEnable(GL_DEPTH_TEST);
-
-        //view matrix
-        glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        int viewLocation = glGetUniformLocation(myShader.ID, "view");
-        glUniformMatrix4fv(viewLocation, 1, false, glm::value_ptr(view));
-
-        //projection matrix
-        glm::mat4 projection;
-        projection = glm::perspective(glm::radians(45.0f), (float)(WINDOW_WIDTH / WINDOW_HEIGHT), 0.1f, 100.0f);
-        int projLocation = glGetUniformLocation(myShader.ID, "projection");
-        glUniformMatrix4fv(projLocation, 1, false, glm::value_ptr(projection));
-
-        //model matrix
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::scale(model, glm::vec3(0.1f, 0.1f, 1.0f));
-        int modelLocation = glGetUniformLocation(myShader.ID, "model");
-        glUniformMatrix4fv(modelLocation, 1, false, glm::value_ptr(model));
-
-        //render triangles
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
-        glDrawElements(GL_LINES, sizeof(indices)/sizeof(*indices), GL_UNSIGNED_INT, 0);
-
-        //check and call events and swap the buffers
-        glfwSwapBuffers(window);
-        glfwPollEvents();    
-    }
-
-    close_window(window);
-
-    //mine!!!
-    /*
-       Shader myShader(vertexCodeString, fragmentCodeString);
-    auto circle = drawCircle(0, 0, 0, 0.6f, 120);
-
-    unsigned int VAO, VBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER,
-                 circle.size() * sizeof(float),
-                 circle.data(),
-                 GL_STATIC_DRAW);
-
-    glVertexAttribPointer(
-        0, 3, GL_FLOAT, GL_FALSE,
-        3 * sizeof(float), (void*)0
-    );
-    glEnableVertexAttribArray(0);
-    */
-    //return 0;
 }
 
 std::vector<float> drawCircle( GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides )
